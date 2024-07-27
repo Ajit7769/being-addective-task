3a997181044fe14f46c43b26c3258ba9cf07dc6d
Ajit Sarwade <103232464+Ajit7769@users.noreply.github.com>
2024-07-27 17:02:42 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const db_URL = 'mongodb://localhost:27017/being-addective'
+const db_URL = 'mongodb+srv://ajitsarwade77:Ajit@7769@cluster0.10k43gg.mongodb.net/being-addective?retryWrites=true&w=majority&appName=Cluster0'
 
 const db_Connection = () =>{
     mongoose.connect(db_URL ,{
